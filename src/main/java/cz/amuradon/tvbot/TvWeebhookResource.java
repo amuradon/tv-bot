4bcd70b9f4590db3373c10ded0ac782cb572843d
Martin Augusta <martin.augusta@gmail.com@>
2026-04-13 10:58:39 +0200

    T #6 test outbound external connection

--- src/main/java/cz/amuradon/tvbot/TvWeebhookResource.java
+++ src/main/java/cz/amuradon/tvbot/TvWeebhookResource.java
@@ -1,5 +1,20 @@
 package cz.amuradon.tvbot;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import com.binance.connector.client.common.configuration.ClientConfiguration;
+import com.binance.connector.client.common.configuration.SignatureConfiguration;
+import com.binance.connector.client.derivatives_trading_usds_futures.rest.DerivativesTradingUsdsFuturesRestApiUtil;
+
 import io.quarkus.logging.Log;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -10,5 +25,17 @@ public class TvWeebhookResource {
 	@POST
 	public void handle(WebhookData data) {
 		Log.infof("Received webhook request %s", data);
+		
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+		         .uri(URI.create("https://ifconfig.me/ip"))
+		         .build();
+		try {
+			HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+			Log.infof("Response %d\n%s", response.statusCode(), response.body());
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
