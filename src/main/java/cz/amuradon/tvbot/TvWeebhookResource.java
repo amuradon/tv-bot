e36362409503db5afbc29d9300a43d5cad35df83
Martin Augusta <martin.augusta@gmail.com@>
2026-04-13 09:06:17 +0200

    T #1 Logging

--- src/main/java/cz/amuradon/tvbot/TvWeebhookResource.java
+++ src/main/java/cz/amuradon/tvbot/TvWeebhookResource.java
@@ -1,5 +1,6 @@
 package cz.amuradon.tvbot;
 
+import io.quarkus.logging.Log;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
@@ -8,6 +9,6 @@ public class TvWeebhookResource {
 
 	@POST
 	public void handle(WebhookData data) {
-		System.out.println(data);
+		Log.infof("Received webhook request %s", data);
 	}
 }
