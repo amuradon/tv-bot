bf2190aaec3bd4ead0df441140d91365067bb075
Martin Augusta <martin.augusta@gmail.com@>
2026-04-13 12:09:57 +0200

    T #8 loading secrets from env variable

--- src/main/java/cz/amuradon/tvbot/TvWeebhookResource.java
+++ src/main/java/cz/amuradon/tvbot/TvWeebhookResource.java
@@ -1,41 +1,39 @@
 package cz.amuradon.tvbot;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-
-import javax.net.ssl.HttpsURLConnection;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import com.binance.connector.client.common.configuration.ClientConfiguration;
 import com.binance.connector.client.common.configuration.SignatureConfiguration;
 import com.binance.connector.client.derivatives_trading_usds_futures.rest.DerivativesTradingUsdsFuturesRestApiUtil;
 
 import io.quarkus.logging.Log;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 @Path("/tvwebhook")
 public class TvWeebhookResource {
 
+	private final String apiKey;
+
+	private final String secretKey;
+	
+	@Inject
+	public TvWeebhookResource(@ConfigProperty(name = "BINANCE_API_KEY")	String apiKey,
+			@ConfigProperty(name = "BINANCE_SECRET_KEY") String secretKey) {
+		this.apiKey = apiKey;
+		this.secretKey = secretKey;
+	}
+	
+	
 	@POST
 	public void handle(WebhookData data) {
 		Log.infof("Received webhook request %s", data);
 		
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder()
-		         .uri(URI.create("https://ifconfig.me/ip"))
-		         .build();
-		try {
-			HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-			Log.infof("Response %d\n%s", response.statusCode(), response.body());
-		} catch (IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		ClientConfiguration config = DerivativesTradingUsdsFuturesRestApiUtil.getClientConfiguration();
+		SignatureConfiguration signatureConfiguration = new SignatureConfiguration();
+		signatureConfiguration.setApiKey(apiKey);
+		signatureConfiguration.setSecretKey(secretKey);
+		config.setSignatureConfiguration(signatureConfiguration);
 	}
 }
