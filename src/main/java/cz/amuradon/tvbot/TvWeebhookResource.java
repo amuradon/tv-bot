6bf1eea30bde5fdbbd0d3bd561392a5715fc8fbb
Martin Augusta <martin.augusta@gmail.com@>
2026-04-15 09:29:38 +0200

    T #9 security

--- src/main/java/cz/amuradon/tvbot/TvWeebhookResource.java
+++ src/main/java/cz/amuradon/tvbot/TvWeebhookResource.java
@@ -1,6 +1,9 @@
 package cz.amuradon.tvbot;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
+import org.jboss.resteasy.reactive.RestResponse.Status;
 
 import com.binance.connector.client.common.configuration.ClientConfiguration;
 import com.binance.connector.client.common.configuration.SignatureConfiguration;
@@ -18,22 +21,33 @@ public class TvWeebhookResource {
 
 	private final String secretKey;
 	
+	private final String userUuid;
+	
 	@Inject
 	public TvWeebhookResource(@ConfigProperty(name = "BINANCE_API_KEY")	String apiKey,
-			@ConfigProperty(name = "BINANCE_SECRET_KEY") String secretKey) {
+			@ConfigProperty(name = "BINANCE_SECRET_KEY") String secretKey,
+			@ConfigProperty(name = "TVBOT_USER_UUID") String userUuid) {
 		this.apiKey = apiKey;
 		this.secretKey = secretKey;
+		this.userUuid = userUuid;
 	}
 	
 	
 	@POST
-	public void handle(WebhookData data) {
-		Log.infof("Received webhook request %s", data);
+	public RestResponse<Object> handle(WebhookData data) {
+		Log.infof("Received webhook request for %s, %s, %s", data.symbol(), data.side(), data.newClientOrderId());
+		
+		if (data.userUuid() == null || !data.userUuid().equals(userUuid)) {
+			Log.errorf("Request for %s, %s, %s unauthorized", data.symbol(), data.side(), data.newClientOrderId());
+			return ResponseBuilder.create(Status.UNAUTHORIZED).build();
+		}
 		
 		ClientConfiguration config = DerivativesTradingUsdsFuturesRestApiUtil.getClientConfiguration();
 		SignatureConfiguration signatureConfiguration = new SignatureConfiguration();
 		signatureConfiguration.setApiKey(apiKey);
 		signatureConfiguration.setSecretKey(secretKey);
 		config.setSignatureConfiguration(signatureConfiguration);
+		
+		return ResponseBuilder.ok().build();
 	}
 }
